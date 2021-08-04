a7d36f2bbc5b938f2b9024dd52ce3b14038013af
caijiabao11 <58868966+caijiabao11@users.noreply.github.com>
2021-08-04 18:58:10 +0800

    q

--- app/src/main/java/com/example/administrator/lztsg/MmdActivity.java
+++ app/src/main/java/com/example/administrator/lztsg/MmdActivity.java
@@ -1,5 +1,7 @@
 package com.example.administrator.lztsg;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
@@ -7,12 +9,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AbsListView;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -21,6 +30,10 @@ public class MmdActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private ImageButton mImgButton;
     private FloatingActionButton mFloatingActionButton;
+    protected static final FrameLayout.LayoutParams COVER_SCREEN_PARAMS = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    private View customView;
+    private FrameLayout fullscreenContainer;
+    private WebChromeClient.CustomViewCallback customViewCallback;
 
 
     @Override
@@ -51,22 +64,43 @@ public class MmdActivity extends AppCompatActivity {
         mWvMain.getSettings().setLoadWithOverviewMode(true); // 缩放至屏幕的大小
         mWvMain.getSettings().setJavaScriptEnabled(true);//支持js
         mWvMain.getSettings().setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
-        mWvMain.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);//缓存设置，直接加载网络数据
+        mWvMain.getSettings().setLoadsImagesAutomatically(true);//支持自动加载图片
+        mWvMain.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//缓存设置，直接加载网络数据
         mWvMain.getSettings().setAllowFileAccess(true); //设置可以访问文件
-        mWvMain.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
         mWvMain.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        mWvMain.getSettings().setAppCacheEnabled(true);//开启H5(APPCache)缓存功能
         mWvMain.getSettings().setBlockNetworkImage(false);
         mWvMain.getSettings().setDefaultTextEncodingName("UTF-8");//编码
         mWvMain.getSettings().setLoadWithOverviewMode(true);
+        mWvMain.getSettings().setAllowUniversalAccessFromFileURLs(true);//允许跨域访问
+        mWvMain.getSettings().setDomStorageEnabled(true);// 开启 DOM storage 功能
         //加载网络url
-        mWvMain.loadUrl("https:/www.iwara.tv/?language=zh-hans");
+        mWvMain.loadUrl("https://www.iwara.tv/?language=zh-hans");
         mWvMain.setVerticalScrollBarEnabled(false);
         mWvMain.setHorizontalScrollBarEnabled(false);
         mWvMain.setWebViewClient(webClient);
-        mWvMain.getSettings().setDomStorageEnabled(false);
-        if (Build.VERSION.SDK_INT >= 21) {//同时允许HTTP和HTTPS
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//同时允许HTTP和HTTPS
             mWvMain.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
+        //视频播放相关的方法
+        mWvMain.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public View getVideoLoadingProgressView() {
+                FrameLayout frameLayout = new FrameLayout(MmdActivity.this);
+                frameLayout.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, GridLayout.LayoutParams.MATCH_PARENT));
+                return frameLayout;
+            }
+
+            @Override
+            public void onShowCustomView(View view, CustomViewCallback callback) {
+                showCustomView(view, callback);
+            }
+
+            @Override
+            public void onHideCustomView() {
+                hideCustomView();
+            }
+        });
     }
 
     //Web视图
@@ -84,15 +118,6 @@ public class MmdActivity extends AppCompatActivity {
         }
     };
 
-    //点击返回上一页面而不是退出浏览器
-    public boolean onKeyDown ( int keyCode, KeyEvent event){
-        if (keyCode == KeyEvent.KEYCODE_BACK && mWvMain.canGoBack()) {
-            mWvMain.goBack();
-            return true;
-        }
-
-        return super.onKeyDown(keyCode, event);
-    }
 
     //返回箭头点击事件
     public void ImageButtonOnClick(){
@@ -103,6 +128,7 @@ public class MmdActivity extends AppCompatActivity {
             }
         });
     }
+
     //悬浮按钮刷新点击事件
     public void FloatingActionButtonreloadOnClick(){
         mFloatingActionButton.setOnClickListener(new View.OnClickListener(){
@@ -112,5 +138,81 @@ public class MmdActivity extends AppCompatActivity {
                 Toast.makeText(MmdActivity.this, "少女祈祷中ing...", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    //视频播放全屏
+    private void showCustomView(View view, WebChromeClient.CustomViewCallback callback) {
+        // if a view already exists then immediately terminate the new one
+        if (customView != null) {
+            callback.onCustomViewHidden();
+            return;
+        }
+        MmdActivity.this.getWindow().getDecorView();
+        FrameLayout decor = (FrameLayout) getWindow().getDecorView();
+        fullscreenContainer = new FullscreenHolder(MmdActivity.this);
+        fullscreenContainer.addView(view, COVER_SCREEN_PARAMS);
+        decor.addView(fullscreenContainer, COVER_SCREEN_PARAMS);
+        customView = view;
+        setStatusBarVisibility(false);
+        customViewCallback = callback;
+    }
+
+    //隐藏视频全屏
+    private void hideCustomView() {
+        if (customView == null) {
+            return;
+        }
+        setStatusBarVisibility(true);
+        FrameLayout decor = (FrameLayout) getWindow().getDecorView();
+        decor.removeView(fullscreenContainer);
+        fullscreenContainer = null;
+        customView = null;
+        customViewCallback.onCustomViewHidden();
+        mWvMain.setVisibility(View.VISIBLE);
+        // 设置竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // 取消全屏
+        final WindowManager.LayoutParams attrs = getWindow().getAttributes();
+        attrs.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setAttributes(attrs);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    // 全屏容器界面
+    static class FullscreenHolder extends FrameLayout {
+
+        public FullscreenHolder(Context ctx) {
+            super(ctx);
+            setBackgroundColor(ctx.getResources().getColor(android.R.color.black));
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent evt) {
+            return true;
+        }
+    }
+
+    //设置全屏
+    private void setStatusBarVisibility(boolean visible) {
+        int flag = visible ? 0 : WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setFlags(flag, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    //点击返回上一页面而不是退出浏览器 优先级:视频播放全屏-网页回退-关闭页面
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                if (customView != null) {
+                    hideCustomView();
+                } else if (keyCode == KeyEvent.KEYCODE_BACK && mWvMain.canGoBack()) {
+                    mWvMain.goBack();
+                } else {
+                    finish();
+                }
+                return true;
+            default:
+                return super.onKeyUp(keyCode, event);
+        }
     }
 }
