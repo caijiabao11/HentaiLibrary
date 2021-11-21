f65a6eb53a8eb09b13a5daa687fb75979f4a6ca7
caijiabao11 <58868966+caijiabao11@users.noreply.github.com>
2021-11-21 18:46:01 +0800

    更新清除缓存功能

--- app/src/main/java/com/example/administrator/lztsg/SettingActivity.java
+++ app/src/main/java/com/example/administrator/lztsg/SettingActivity.java
@@ -3,8 +3,10 @@ package com.example.administrator.lztsg;
 import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.Preference;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class SettingActivity extends AppCompatActivity {
@@ -34,16 +36,45 @@ public class SettingActivity extends AppCompatActivity {
 
 
     public static class SettingsFragment extends PreferenceFragment {
+        private Preference mSummary;
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+            getCaheSize();
+        }
 
+        @Override
+        public boolean onPreferenceTreeClick(Preference preference) {
+            if ("setting_cleandate".equals(preference.getKey())){
+                Toast.makeText(getActivity(),"嗯，整洁了不少呢,,,帮大忙了！！", Toast.LENGTH_SHORT).show();
+                cleanCache();
+                mSummary.setSummary(getCaheSize());
+            }
+            return super.onPreferenceTreeClick(preference);
         }
 
         @Override
         public void onCreatePreferences(Bundle bundle, String s) {
 
+        }
+
+        //获取缓存大小
+        private String getCaheSize(){
+            String str="";
+            try{
+                str = DataCleanManager.getTotalCacheSize(getActivity());
+                mSummary = findPreference("setting_cleandate");
+                mSummary.setSummary(str);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        //清除缓存
+        private void cleanCache(){
+            DataCleanManager.clearAllCache(getActivity());
         }
     }
 
